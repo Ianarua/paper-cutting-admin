a49ab8b2b3bc78942e6ff7f34224edcd31797892
Ianarua <3067815232@qq.com>
2024-05-09 15:46:04 +0800

    delete: 后端文件

--- paper-admin-background/paper-admin/src/main/java/com/iyaovo/paper/admin/mapper/GoodsCategoryMapper.java
+++
@@ -1,7 +0,0 @@
-package com.iyaovo.paper.admin.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.iyaovo.paper.admin.domain.entity.GoodsCategory;
-
-public interface GoodsCategoryMapper extends BaseMapper<GoodsCategory> {
-}
